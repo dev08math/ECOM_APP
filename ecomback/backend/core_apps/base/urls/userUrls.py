a665f1c967bf8c7a95e0d50a91d3cd648ad7eff1
Mathew Prince <mathewmanjooran08@gmail.com>
2022-09-13 05:20:01 -0400

    Updated user views and urls

--- ecomback/backend/core_apps/base/urls/userUrls.py
+++ ecomback/backend/core_apps/base/urls/userUrls.py
@@ -1,14 +1,28 @@
 from django.urls import path
 
-from ..views.userViews import CustomTokenObtainPairView, getUserProfile, getUsers, updateProfile, registerUser
+from ..views.userViews import (
+    CustomTokenObtainPairView,
+    getUserProfile,
+    getUsers,
+    updateProfile,
+    registerUser,
+    deleteUser,
+    getUserById,
+    updateUser,
+)
 
 
 urlpatterns = [
-    path('', getUsers.as_view(), name='get_all_users'),
-    path('profile', getUserProfile.as_view(), name="user_profile"),
+    path("profile", getUserProfile.as_view(), name="user_profile"),
+    path("profile/update", updateProfile.as_view(), name="update"),
 
-    path('profile/update', updateProfile.as_view(), name="update"),
-    path('register', registerUser.as_view(), name='register'),
+    path("register", registerUser.as_view(), name="register"),
 
-    path('login', CustomTokenObtainPairView.as_view(), name='obtain_jwt_pair'),
+    path("login", CustomTokenObtainPairView.as_view(), name="obtain_jwt_pair"),
+
+    path("", getUsers.as_view(), name="get_all_users"),
+    path("update/<int:id>", updateUser.as_view(), name="update-user"),
+    path("delete/<int:id>", deleteUser.as_view(), name="delete-user"),
+    path("<int:id>", getUserById.as_view(), name="get-user-details"),
+    
 ]
--- ecomback/backend/core_apps/base/views/userViews.py
+++ ecomback/backend/core_apps/base/views/userViews.py
@@ -73,3 +73,27 @@ class getUsers(APIView):
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+class getUserById(APIView):
+    permission_classes = [Isadmin]
+    def get(self, request, id):
+        user = User.objects.get(id == id)
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class updateUser(APIView):
+    permission_classes = [Isadmin]
+    def patch(self, request, id):
+        user = User.objects.get(id == id)
+        data = request.data
+
+        user.isAdmin = data['isAdmin']
+        serializer = UserSerializer(instance=user,  data=request.data, partial=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class deleteUser(APIView):
+    permission_classes = [Isadmin]
+    def get(self, request, id):
+        user = User.objects.get(id == id)
+        if user.IsAdmin : return Response({'detail' : 'Cannot delete an Admin'}, status=status.HTTP_400_BAD_REQUEST)
+        user.delete()
+        return Response("User was deleted.", status=status.HTTP_200_OK)
