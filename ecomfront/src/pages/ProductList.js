ba120a425a0f6261d3f1911baa9f73338c004647
Mathew Prince <mathewprince.dev08@gmail.com>
2022-09-14 01:15:28 +0530

    updated

--- ecomfront/src/pages/ProductList.js
+++ ecomfront/src/pages/ProductList.js
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+// import { LinkContainer } from 'react-router-bootstrap'
+// import { Table, Button, Row, Col } from 'react-bootstrap'
+// import { useDispatch, useSelector } from 'react-redux'
+// import Loader from '../components/Loader'
+// import Message from '../components/Message'
+// import Paginate from '../components/Paginate'
+// import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
+// import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 function ProductList() {
   return (
