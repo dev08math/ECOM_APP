75d5a708ec4755e0f3e0b4496db5957a4764a006
Mathew Prince <mathewmanjooran08@gmail.com>
2022-09-14 05:07:30 -0400

    Updated product views and urls

--- ecomback/backend/core_apps/base/urls/productUrls.py
+++ ecomback/backend/core_apps/base/urls/productUrls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 
-from ..views.productViews import getAllProducts, getAProduct
+from ..views.productViews import (
+    getAllProducts,
+    getAProduct,
+    deleteProduct,
+    updateProduct,
+    createProduct,
+)
 
 urlpatterns = [
     path("", getAllProducts.as_view(), name="all-products"),
     path("<int:id>", getAProduct.as_view(), name="a-product"),
+
+    path("create", createProduct.as_view(), name="create-product"),
+    path("update/<int:id>", updateProduct.as_view(), name="create-product"),
+    path("delete/<int:id>", deleteProduct.as_view(), name="delete-prod")
+
 ]
--- ecomback/backend/core_apps/base/views/productViews.py
+++ ecomback/backend/core_apps/base/views/productViews.py
@@ -3,12 +3,13 @@ from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
 from rest_framework import status
 
-
-# from .products_list import products
 from ..serializers import ProductSerializer
 from ..models import Product
 
+from ..adminpermission import Isadmin
+
 class getAllProducts(APIView):
+    # have to update it with pagination
     serializer_class = ProductSerializer
     products = Product.objects.all()
 
@@ -26,4 +27,44 @@ class getAProduct(APIView):
             
         serializer = self.serializer_class(product)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class createProduct(APIView):
+    permission_classes = [Isadmin]
+    def post(self, request):
+        
+        try:
+            product = Product.objects.create(
+            user=request.user,
+            name='Enter Product Name',
+            price=0,
+            brand='Enter Brand Name',
+            countInStock=0,
+            category='Enter Category',
+            description=''
+            )
+        except:
+            return Response({'detail' : 'Unable to create a new product'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = self.serializer_class(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class updateProduct(APIView):
+    permission_classes = [Isadmin]
+    def patch(self, request, id):
+        data = request.data
+        product = Product.objects.get(_id=id)
+
+        serializer = ProductSerializer(instance=product, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+        else: return Response({'detail' : 'Unable to edit the product details. Kindly check and re do it.'})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class deleteProduct(APIView):
+    permission_classes = [Isadmin]
+    def delete(self, request, id):
+        product = Product.objects.get(_id=id)
+        product.delete()
+        return Response("The product is now removed from the store.", status=status.HTTP_200_OK)
+
 
