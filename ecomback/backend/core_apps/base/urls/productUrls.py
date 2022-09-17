1302750b9faed14934a089eb4aa2a72ed6c3c21d
Mathew Prince <mathewprince.dev08@gmail.com>
2022-09-18 00:08:58 +0530

    Updated with reviews

--- ecomback/backend/core_apps/base/urls/productUrls.py
+++ ecomback/backend/core_apps/base/urls/productUrls.py
@@ -1,3 +1,4 @@
+from ssl import cert_time_to_seconds
 from django.urls import path
 
 from ..views.productViews import (
@@ -6,14 +7,18 @@ from ..views.productViews import (
     deleteProduct,
     updateProduct,
     createProduct,
+    getTopProducts,
+    createProductReview
 )
 
 urlpatterns = [
     path("", getAllProducts.as_view(), name="all-products"),
     path("<int:id>", getAProduct.as_view(), name="a-product"),
+    path("top", getTopProducts.as_view(), name="top-products"),
 
     path("create", createProduct.as_view(), name="create-product"),
     path("update/<int:id>", updateProduct.as_view(), name="create-product"),
-    path("delete/<int:id>", deleteProduct.as_view(), name="delete-prod")
+    path("delete/<int:id>", deleteProduct.as_view(), name="delete-prod"),
+    path("reviews/<int:id>", createProductReview.as_view(), name="review")
 
 ]
--- ecomback/backend/core_apps/base/views/productViews.py
+++ ecomback/backend/core_apps/base/views/productViews.py
@@ -1,20 +1,26 @@
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status, permissions
 
 from ..serializers import ProductSerializer
-from ..models import Product
+from ..models import Product, Review
+
 
 from ..adminpermission import Isadmin
 
 class getAllProducts(APIView):
     # have to update it with pagination
     serializer_class = ProductSerializer
-    products = Product.objects.all()
 
-    def get(self, reuquest):
-        serializer = self.serializer_class(self.products, many=True)
+    def get(self, request):
+        query = request.query_params.get('searchkey')
+        if query == None:
+            query = ''
+  
+        products = Product.objects.filter(name__icontains=query).order_by('-createdAt')
+
+        serializer = self.serializer_class(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class getAProduct(APIView):
@@ -30,6 +36,7 @@ class getAProduct(APIView):
 
 class createProduct(APIView):
     permission_classes = [Isadmin]
+    serializer_class = ProductSerializer
     def post(self, request):
         
         try:
@@ -53,10 +60,12 @@ class updateProduct(APIView):
     def patch(self, request, id):
         data = request.data
         product = Product.objects.get(_id=id)
-
+        # print(product._id, " ", product.name)
+        # print(data)
         serializer = ProductSerializer(instance=product, data=data, partial=True)
         if serializer.is_valid():
             serializer.save()
+            
         else: return Response({'detail' : 'Unable to edit the product details. Kindly check and re do it.'})
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -66,5 +75,50 @@ class deleteProduct(APIView):
         product = Product.objects.get(_id=id)
         product.delete()
         return Response("The product is now removed from the store.", status=status.HTTP_200_OK)
+
+class getTopProducts(APIView):
+    def get(self, request):
+        products = Product.objects.filter(rating__gte=4).order_by('-rating')[0:5]
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class createProductReview(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def post(self, request, id):
+        user = request.user
+        product = Product.objects.get(_id=id)
+        data = request.data
+
+        alreadyExists = product.review_set.filter(user=user).exists()
+        if alreadyExists:
+            content = {'detail': 'Product already reviewed'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+        # 2 - No Rating or 0
+        elif data['rating'] == 0:
+            content = {'detail': 'Please select a rating'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+        # 3 - Create review
+        else:
+            review = Review.objects.create(
+                user=user,
+                product=product,
+                name=user.first_name,
+                rating=data['rating'],
+                comment=data['comment'],
+            )
+
+            reviews = product.review_set.all()
+            product.numReviews = len(reviews)
+
+            total = 0
+            for i in reviews:
+                total += i.rating
+
+            product.rating = total / len(reviews)
+            product.save()
+
+            return Response('Review Added', status=status.HTTP_200_OK)
 
 
